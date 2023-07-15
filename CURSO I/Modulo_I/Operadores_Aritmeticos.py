9da2cec10edb33d808c3d7dae7acff298c07ecc5
Your Name <you@example.com>
2023-07-15 02:12:08 -0400

    Operadores_Aritmeticos.py listo

---
+++ CURSO I/Modulo_I/Operadores_Aritmeticos.py
@@ -0,0 +1,16 @@
+x = 100
+y = 10
+print("Valor de x = ", x)
+print("Valor de y = ", y)
+print("X+Y = ", x+y)
+print("X-Y = ", x-y)
+print("X*Y = ", x*y)
+
+x, y = 13, 2
+print("Division flotante", "X/Y = ", x/y)
+print("Division entera", "X//Y = ", x//y)
+print("Division modulo", "X%Y = ", x%y)
+x, y = 2, 3
+print("POTENCiA", "X**Y = ", x**y)
+
+
