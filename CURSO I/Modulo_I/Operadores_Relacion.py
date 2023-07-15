b56594b99e706eeb65d38294ba2ac047a5c8fcb0
Your Name <you@example.com>
2023-07-15 02:19:08 -0400

    Operadores_Relacion.py completo

---
+++ CURSO I/Modulo_I/Operadores_Relacion.py
@@ -0,0 +1,24 @@
+print("OPERADORES DE RELACION CON NUMEROS")
+x = 35
+y = 30
+
+print("x == y", x == y)
+print("x != y", x != y)
+print("x < y", x < y)
+print("x <= y", x <= y)
+
+print("x > y", x > y)
+print("x >= y", x >= y)
+
+print("OPERADORES DE RELACION CON STR")
+x = "JUAN"
+y = "JUAN"
+
+print("x == y", x == y)
+print("x != y", x != y)
+print("x < y", x < y)
+print("x <= y", x <= y)
+
+print("x > y", x > y)
+print("x >= y", x >= y)
+
