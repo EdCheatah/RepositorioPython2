1230a7981d8fd64b0209fbd5efe6cae7d66d2528
Your Name <you@example.com>
2023-07-15 02:02:10 -0400

    Operadores_Asignacion.py completo

---
+++ CURSO I/Modulo_I/Operadores_Asignacion.py
@@ -0,0 +1,15 @@
+print("SIGNOS: =")
+z = 40
+z = z + 10
+print(z)
+
+variable1, variable2, variable3 = 1, "TEXTO", None
+
+print(variable1)
+print(type(variable1))
+
+print(variable2)
+print(type(variable2))
+
+print(variable3)
+print(type(variable3))
