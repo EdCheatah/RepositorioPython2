92779a07e8dbc3770fc9cf3dac982e51e4bcb8b6
Your Name <you@example.com>
2023-07-16 22:25:28 -0400

    Condicional_Multiple.py agregado

---
+++ CURSO I/Modulo_II/Condicional_Multiple.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingrese un número: "))
+if numero > 0:
+    print("Ingresaste un número positivo")
+elif numero < 0:
+    print("Ingresaste un número negativo")
+else:
+    print("Ingresaste cero")
