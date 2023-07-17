1f7d77c1054fd8a81a67efbb3173d1995ff7fd9c
Your Name <you@example.com>
2023-07-16 22:34:19 -0400

    Condicionales_Anidadas.py agregado

---
+++ CURSO I/Modulo_II/Condicionales_Anidadas.py
@@ -0,0 +1,10 @@
+#Combinacion de las tres selectivas
+#Simple, múltiple y doble
+edad = int(input("Ingresar edad: "))
+if edad >= 18:
+    print("Mayor de edad")
+else:
+    if edad > 0:
+        print("Menor de edad")
+    else:
+        print("Edad no correcta")
